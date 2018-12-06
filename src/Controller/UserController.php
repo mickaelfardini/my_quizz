51a3bada0338b4c6f6b7ea501ece679c7cb2360e
mickaelfardini <mickael.fardini@epitech.eu>
2018-12-06 16:15:52 +0100

    update user, email confirmation on change

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
@@ -23,21 +24,35 @@ class UserController extends AbstractController
 	/**
 	 * @Route("/profile/edit", name="user.edit")
 	 */
-	public function edit(Request $request)
+	public function edit(Request $request, UserPasswordEncoderInterface $encoder, \Swift_Mailer $mailer)
 	{
 		$user = $this->getUser();
+		$old_email = $user->getEmail();
+		// dump();
+		dump($user);
+		// die;
 		$form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
+		$form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+		if ($form->isSubmitted() && $form->isValid()) {
+			if ($request->get('user')['email'] != $old_email) {
+				$user->setToken(md5(mt_rand()));
+				$user->setActive(0);
+				$message = (new \Swift_Message('Confirm new Email'))
+				->setFrom('noreply@localhost')->setTo($user->getEmail())
+				->setBody($this->renderView('email/register.html.twig', [
+					"token" => $user->getToken() ]));
+				$mailer->send($message);
+			}
+			$user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+			$this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('user_edit', ['id' => $user->getId()]);
-        }
+			return $this->redirectToRoute('user.index', ['id' => $user->getId()]);
+		}
 
-        return $this->render('user/edit.html.twig', [
-            'user' => $user,
-            'form' => $form->createView(),
-        ]);
+		return $this->render('user/edit.html.twig', [
+			'user' => $user,
+			'form' => $form->createView(),
+		]);
 	}
 }
